f5aca3f1aba5590ff15c081b7453cc56f91441ca
Roman <savroful@gmail.com>
2022-03-24 14:06:15 +0300

    change allowed hosts

--- to_do/settings.py
+++ to_do/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.environ.get('DEBUG', True)
 # if not DEBUG:
 #     ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
 
-ALLOWED_HOSTS = ['127.0.0.1', 'tasker2.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'first-web-app-to-do.herokuapp.com']
 
 # Application definition
 
